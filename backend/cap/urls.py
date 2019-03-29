cf47bc8bc859b221a5f9fb5355586baf60870d0c
CFE <hello@teamcfe.com>
2019-03-29 13:11:41 -0700

    9 - Templates and Home View

--- backend/cap/urls.py
+++ backend/cap/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', TemplateView.as_view(template_name='home.html')),
 ]
