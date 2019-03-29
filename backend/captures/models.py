3ad933b76b74a9034163629ab054ade491bae564
CFE <hello@teamcfe.com>
2019-03-29 11:27:43 -0700

    6 - Email Captures and Captures App

---
+++ backend/captures/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+
+User = settings.AUTH_USER_MODEL
+
+
+# models.SET_NULL set all foreigns null if Foreign Key Object is deleted
+# models.CASCADE - deleted related objects
+class EmailCapture(models.Model):
+    user            = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    email           = models.EmailField()
+    # verified        = models.BooleanField(default=False)
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    # updated         = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
