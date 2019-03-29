f6109e2f4de8b56dd839888010ec01a41f58f836
CFE <hello@teamcfe.com>
2019-03-29 13:48:51 -0700

    13 - Authentication and Permissions

--- backend/captures/views.py
+++ backend/captures/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import authentication, generics, permissions
 
 from .models import EmailCapture
 from .serializers import EmailCaptureSerializer
@@ -6,7 +6,8 @@ from .serializers import EmailCaptureSerializer
 class EmailCaptureCreateAPIView(generics.CreateAPIView):
     queryset            = EmailCapture.objects.all()
     serializer_class    = EmailCaptureSerializer
-
+    authentication_classes =  [authentication.SessionAuthentication]
+    permission_classes = [permissions.AllowAny]
     def perform_create(self, serializer):
         request = self.request
         user = request.user
