6fad46841780fa525e6977b44525dec7f9a273ae
CFE <hello@teamcfe.com>
2019-03-29 14:33:58 -0700

    14 - Create React App Method 1

--- backend/cap/settings.py
+++ backend/cap/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-STATIC_URL = '/static-abc/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'cdn_local', 'static')
