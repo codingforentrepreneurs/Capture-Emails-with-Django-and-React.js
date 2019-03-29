a1d6b6b721c1b4121ecc76bcff211bd22f41424a
CFE <hello@teamcfe.com>
2019-03-29 14:15:17 -0700

    model notes

--- backend/captures/models.py
+++ backend/captures/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+# models.SET_NULL set all foreigns null if Foreign Key Object is deleted
+# models.CASCADE - deleted related objects to this foreign key 
+# Casade is if user is deleted, so is the instances that relate to it
+# models.CASCADE
 class EmailCapture(models.Model):
     user            = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     email           = models.EmailField()
